9ccc44b652eab5430e8d18b20a6dcd5d42e6fd9f
Amandeep Nehra <55580017+amandeep-nehra@users.noreply.github.com>
2022-01-15 16:21:27 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+
+?>
+
+
+<html>
+    <head>
+        <title>Welcome to oure website </title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+        <link type="text/css" rel="stylesheet" href="css/style.css">
+    </head>
+    <style type="text/css">
+    body{
+      margin:0;
+      padding:0;
+      font-family: 'Times New Roman', Times, serif;
+      background: url('C:\xampp\htdocs\Stocks_Management\img\pexels-photo-6120213.png') no-repeat;
+      background-size: cover
+    }
+    </style>
+
+    <body>
+        <div>
+            <p class="center_align">
+                Welcome to your inventory!
+            </p>
+        </div>
+
+        <div class="col-sm-6">
+            <a href="login.php">
+                <input  type="button" class="btn btn-danger pull-right" value="Login">
+            </a>
+        </div>
+
+        <div class="col-sm-6">
+            <a href="register.php">
+                <input  type="button" class="btn btn-success pull-left" value="Register">
+            </a>
+        </div>
+        <div class="copyright">
+<i class="far fa-copyright"></i><br>
+<h3>Amandeep and Amulya</h3>
+</div>
+    </body>
+
+</html>
