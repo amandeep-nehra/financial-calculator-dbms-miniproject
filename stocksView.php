c80604cdd74543bba69bbbbd780fd7985ed6181b
Amandeep Nehra <55580017+amandeep-nehra@users.noreply.github.com>
2022-01-17 18:01:04 +0530

    Add files via upload

---
+++ stocksView.php
@@ -0,0 +1,130 @@
+<?php
+session_start();
+include 'navigation.php';
+
+$conn = connect();
+$id = $_SESSION['userid'];
+$sq = "SELECT * FROM user_login WHERE user_id='$id'";
+$thisUser = mysqli_fetch_assoc($conn->query($sq));
+// change rd_id to __id
+if (isset($_GET['stock_id'])) {
+    $id = $_GET['stock_id'];
+
+    $sql = "SELECT * from stocks WHERE stock_id=$id limit 1";
+    $res = mysqli_fetch_assoc($conn->query($sql));
+}
+?>
+
+<html>
+
+<head>
+    <title> STOCKS </title>
+    <!-- change title -->
+    <link rel="stylesheet" type="text/css" href="css/products.css">
+</head>
+
+<body>
+    <div class="row" style="padding-top: 50px;">
+        <div class="leftcolumn">
+            <div class="row">
+                <section style="padding-left: 20px; padding-right: 20px;">
+                    <div class="col-sm-3">
+                        <div class="card card-green">
+                            <a href="rdcal.php">RD Calculator</a>
+                        </div>
+                    </div>
+                    <div class="col-sm-3">
+                        <div class="card card-yellow">
+                            <a href="fdcal.php">FD Calculator</a>
+                        </div>
+                    </div>
+                    <div class="col-sm-3 ">
+                        <div class="card card-blue">
+                            <a href="ppfcal.php">PPF Calculator</a>
+                        </div>
+                    </div>
+                    <div class="col-sm-3">
+                        <div class="card card-red">
+                            <a href="stockscal.php">Stocks Calculator</a>
+                        </div>
+                    </div>
+                </section>
+            </div>
+
+            <div class="pt-20 pl-20">
+                <div class="col-sm-12" style="background-color: #282828; ">
+                    <div class="text-center">
+                        <!-- add changes -->
+                        <h2> Stocks Investment Plan Details</h2>
+                    </div>
+                    <div class="row pt-20">
+                        <div class="col-sm-7">
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <label class="pull-left">
+                                        <h2> Stock Name:</h2>
+                                    </label>
+                                </div>
+                                <div class="col-sm-6">
+                                    <h2 style="color: whitesmoke;"><?php echo ucwords($res['stock_name']) ?></h2>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <label class="pull-left">
+                                        <h2> Opening Price:</h2>
+                                    </label>
+                                </div>
+                                <div class="col-sm-6">
+                                    <h2 style="color: whitesmoke;"><?php echo $res['open_price'] ?></h2>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <label class="pull-left">
+                                        <h2> Closing Price:</h2>
+                                    </label>
+                                </div>
+                                <div class="col-sm-6">
+                                    <h2 style="color: whitesmoke;"><?php echo $res['close_price'] ?></h2>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <label class="pull-left">
+                                        <h2> Dividend:</h2>
+                                    </label>
+                                </div>
+                                <div class="col-sm-6">
+                                    <h2 style="color: whitesmoke;"><?php echo $res['dividend'] ?></h2>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <label class="pull-left">
+                                        <h2> Stocks Return:</h2>
+                                    </label>
+                                </div>
+                                <div class="col-sm-6">
+                                    <h2 style="color: whitesmoke;"><?php echo $res['stock_return'] ?></h2>
+                                </div>
+                            </div>
+
+
+                            <div class="row text-center">
+                            <!-- change rd_id to __id, change even the links-->
+                                <a href="stocksEdit.php?stock_id=<?php echo $res['stock_id']; ?>"><button class="btn btn-warning">Edit</button></a>
+                                <a href="stocksDelete.php?stock_id=<?php echo $res['stock_id']; ?>"><button class="btn btn-danger">Delete</button></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    </div>
+    <?php include('footer.php') ?>
+</body>
+
+</html>
