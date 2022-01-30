bd7bf88cbcdf9f12a91747a834a643dd2f89985d
amulyal <amulyal333@gmail.com>
2022-01-30 20:38:54 +0530

    Final Commit

---
+++ fdEdit.php
@@ -0,0 +1,172 @@
+<?php
+session_start();
+include 'navigation.php';
+
+$conn = connect();
+$m = '';
+
+$id = $_SESSION['userid'];
+$sq = "SELECT * FROM user_login WHERE user_id='$id'";
+$thisUser = mysqli_fetch_assoc($conn->query($sq));
+// change rdId
+if (isset($_GET['fd_id'])) {
+    $fdId = $_GET['fd_id'];
+    $sql = "SELECT * from fd WHERE fd_id='$fdId' limit 1";
+    $res = mysqli_fetch_assoc($conn->query($sql));
+}
+// change everything including links VERY CAREFULLY!!!!
+elseif (isset($_POST['fd_id'])) {
+    $fdId = $_POST['fd_id'];
+    $bankName = $_POST['bank_name'];
+    $fdComp = intval($_POST['compounding']);
+    $fdPrin = floatval($_POST['fd_prin']);
+    $fdDur = floatval($_POST['fd_dur']);
+    $fdRate = floatval($_POST['fd_rate']);
+
+    if (isset($_POST['Submit'])) {
+        $sql = "UPDATE fd SET bank_name= '$bankName', fd_prin= '$fdPrin', fd_dur= '$fdDur', fd_rate= '$fdRate', compounding='$fdComp' WHERE fd_id = '$fdId';";
+        if ($conn->query($sql) === true) {
+            header('Location: fdcal.php');
+        } else {
+            $m = "Connection Failure!";
+            header("Location: fdEdit.php?fd_id=$fdId");
+        }
+    }
+}
+// make sure you have floatval()
+?>
+
+<html>
+
+<head>
+    <title> FD </title>
+    <!-- change title -->
+    <link rel="stylesheet" type="text/css" href="css/products.css">
+</head>
+
+<body>
+    <div class="row" style="padding-top: 50px;">
+        <div class="leftcolumn">
+            <div class="row">
+                <section style="padding-left: 20px; padding-right: 20px;">
+                    <div class="col-sm-3">
+                        <div class="card card-yellow">
+                            <a href="rdcal.php">RD Calculator</a>
+                        </div>
+                    </div>
+                    <div class="col-sm-3">
+                        <div class="card card-yellow">
+                            <a href="fdcal.php">FD Calculator</a>
+                        </div>
+                    </div>
+                    <div class="col-sm-3 ">
+                        <div class="card card-yellow">
+                            <a href="ppfcal.php">PPF Calculator</a>
+                        </div>
+                    </div>
+                    <div class="col-sm-3">
+                        <div class="card card-yellow">
+                            <a href="stockscal.php">Stocks Calculator</a>
+                        </div>
+                    </div>
+                </section>
+            </div>
+            <div class="pt-20 pl-20">
+                <datalist id="complist">
+                    <option value="1">
+                    <option value="2">
+                    <option value="4">
+                    <option value="12">
+                </datalist>
+                <div class="col-sm-12">
+                    <div class="text-center">
+                        <h2> Edit FD </h2>
+                        <!-- change title -->
+                        <h4> <?php echo $m; ?> </h4>
+                    </div>
+                    <div class="row pt-20">
+                        <div class="col-sm-7">
+                            <form method="POST" action="fdEdit.php">
+                                <!-- edit link above -->
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <label class="pull-left">
+                                            <h4> Bank Name:</h4>
+                                            <!-- from here, change all h4-->
+                                        </label>
+                                    </div>
+                                    <div class="col-sm-6 form-input pt-10">
+                                        <!-- change name, value, placeholder -->
+                                        <input type="text" class="login-input" name="bank_name" value="<?php echo $res['bank_name']; ?>" placeholder="Bank Name">
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <label class="pull-left">
+                                            <h4> FD Principal:</h4>
+                                        </label>
+                                    </div>
+                                    <div class="col-sm-6 form-input pt-10">
+                                        <input type="number" class="login-input" name="fd_prin" value="<?php echo $res['fd_prin']; ?>" placeholder="FD Principal">
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <label class="pull-left">
+                                            <h4> FD Annual Rate of Interest:</h4>
+                                        </label>
+                                    </div>
+                                    <div class="col-sm-6 form-input pt-10">
+                                        <input type="number" class="login-input" name="fd_rate" value="<?php echo $res['fd_rate']; ?>" placeholder="FD Annual Rate of Interest">
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <label class="pull-left">
+                                            <h4> FD Duration (in Years):</h4>
+                                        </label>
+                                    </div>
+                                    <div class="col-sm-6 form-input pt-10">
+                                        <input type="number" class="login-input" name="fd_dur" value="<?php echo $res['fd_dur']; ?>" placeholder="FD Duration">
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <label class="pull-left">
+                                            <h4> Compounding: </h4>
+                                        </label>
+                                    </div>
+                                    <div class="col-sm-6 form-input pt-10">
+                                        <input type="number" class="login-input" name="compounding" list="complist" value="<?php echo $res['compounding']; ?>" placeholder="Compounding">
+                                    </div>
+                                </div>
+                                <!-- here change rdId after echo and again name -->
+                                <input type="hidden" value="<?php echo $fdId; ?>" name="fd_id">
+                                <div class="row">
+                                    <div class="text-center">
+                                        <input class="btn btn-success" type="submit" name="Submit" value="Submit">
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="rightcolumn">
+            <div class="card text-center">
+                <h2><b>User Profile</b></h2>
+                <p>
+                    Logged in as user:
+                <h4><?php echo $thisUser['name'];  ?></h4>
+                <p> Your profile was created on: </p>
+                <h4><?php echo date('F j, Y', strtotime($thisUser['created_at'])); ?></h4>
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <?php include('footer.php') ?>
+</body>
+
+</html>
---
+++ ppfView.php
@@ -0,0 +1,140 @@
+<?php
+session_start();
+include 'navigation.php';
+
+$conn = connect();
+$id = $_SESSION['userid'];
+$sq = "SELECT * FROM user_login WHERE user_id='$id'";
+$thisUser = mysqli_fetch_assoc($conn->query($sq));
+// change rd_id to __id
+if (isset($_GET['ppf_id'])) {
+    $id = $_GET['ppf_id'];
+
+    $sql = "SELECT * from ppf WHERE ppf_id=$id limit 1";
+    $res = mysqli_fetch_assoc($conn->query($sql));
+}
+?>
+
+<html>
+
+<head>
+    <title> PPF </title>
+    <!-- change title -->
+    <link rel="stylesheet" type="text/css" href="css/products.css">
+</head>
+
+<body>
+    <div class="row" style="padding-top: 50px;">
+        <div class="leftcolumn">
+            <div class="row">
+                <section style="padding-left: 20px; padding-right: 20px;">
+                    <div class="col-sm-3">
+                        <div class="card card-yellow">
+                            <a href="rdcal.php">RD Calculator</a>
+                        </div>
+                    </div>
+                    <div class="col-sm-3">
+                        <div class="card card-yellow">
+                            <a href="fdcal.php">FD Calculator</a>
+                        </div>
+                    </div>
+                    <div class="col-sm-3 ">
+                        <div class="card card-yellow">
+                            <a href="ppfcal.php">PPF Calculator</a>
+                        </div>
+                    </div>
+                    <div class="col-sm-3">
+                        <div class="card card-yellow">
+                            <a href="stockscal.php">Stocks Calculator</a>
+                        </div>
+                    </div>
+                </section>
+            </div>
+
+            <div class="pt-20 pl-20">
+                <div class="col-sm-12">
+                    <div class="text-center">
+                        <!-- add changes -->
+                        <h2> PPF Investment Plan Details</h2>
+                    </div>
+                    <div class="row pt-20">
+                        <div class="col-sm-7">
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <label class="pull-left">
+                                        <h4> Bank Name:</h4>
+                                    </label>
+                                </div>
+                                <div class="col-sm-6">
+                                    <h4 ><?php echo ucwords($res['bank_name']) ?></h4>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <label class="pull-left">
+                                        <h4> PPF Principal:</h4>
+                                    </label>
+                                </div>
+                                <div class="col-sm-6">
+                                    <h4 ><?php echo $res['ppf_prin'] ?></h4>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <label class="pull-left">
+                                        <h4> PPF Rate:</h4>
+                                    </label>
+                                </div>
+                                <div class="col-sm-6">
+                                    <h4 ><?php echo $res['ppf_rate'] ?></h4>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <label class="pull-left">
+                                        <h4> PFF Duration in Years (Min 15 years):</h4>
+                                    </label>
+                                </div>
+                                <div class="col-sm-6">
+                                    <h4 ><?php echo $res['ppf_year'] ?></h4>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <label class="pull-left">
+                                        <h4> PPF Return:</h4>
+                                    </label>
+                                </div>
+                                <div class="col-sm-6">
+                                    <h4 ><?php echo $res['ppf_return'] ?></h4>
+                                </div>
+                            </div>
+
+
+                            <div class="row text-center">
+                                <!-- change rd_id to __id, change even the links-->
+                                <a href="ppfEdit.php?ppf_id=<?php echo $res['ppf_id']; ?>"><button class="btn btn-warning">Edit</button></a>
+                                <a href="ppfDelete.php?ppf_id=<?php echo $res['ppf_id']; ?>"><button class="btn btn-danger">Delete</button></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="rightcolumn">
+            <div class="card text-center">
+                <h2><b>User Profile</b></h2>
+                <p>
+                    Logged in as user:
+                <h4><?php echo $thisUser['name'];  ?></h4>
+                <p> Your profile was created on: </p>
+                <h4><?php echo date('F j, Y', strtotime($thisUser['created_at'])); ?></h4>
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <?php include('footer.php') ?>
+</body>
+
+</html>
