3c3cb675a701d57a11dfc7e1d6249784970e26fe
amulyal <amulyal333@gmail.com>
2022-02-12 15:09:53 +0530

    Database updated

--- DB/investments.sql
+++ DB/investments.sql
@@ -1,63 +1,58 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
 --
--- Table structure for table `user_login`
+-- Host: localhost:3307
+-- Generation Time: Feb 12, 2022 at 10:36 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `investments`
 --
 
-DROP TABLE IF EXISTS `user_login`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `user_login` (
-  `user_id` int(11) NOT NULL,
-  `username` varchar(45) NOT NULL,
-  `password` varchar(45) DEFAULT NULL,
-  `email` varchar(70) DEFAULT NULL,
-  `is_active` tinyint(1) DEFAULT 1,
-  `is_admin` tinyint(1) NOT NULL DEFAULT 0,
-  `last_login_time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  `name` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-ALTER TABLE `user_login`
-  ADD PRIMARY KEY (`user_id`),
-  ADD UNIQUE KEY `username_UNIQUE` (`username`),
-  ADD UNIQUE KEY `email_UNIQUE` (`email`);
+DELIMITER $$
 --
--- AUTO_INCREMENT for table `user_login`
+-- Procedures
 --
-ALTER TABLE `user_login`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-COMMIT;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `neg_expo` (INOUT `var` DECIMAL(10,4))  BEGIN
+SET var = POW(var,-1/3);
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `fd`
 --
 
-DROP TABLE IF EXISTS `fd`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `fd` (
-  `fd_id` int NOT NULL AUTO_INCREMENT,
+  `fd_id` int(11) NOT NULL,
   `bank_name` varchar(70) NOT NULL,
-  `user_id` int NOT NULL,
+  `user_id` int(11) NOT NULL,
   `fd_prin` decimal(10,2) DEFAULT NULL,
   `fd_rate` decimal(10,2) DEFAULT NULL,
-  `fd_dur` int DEFAULT NULL,
-  `compounding` int DEFAULT '1',
-  `fd_return` decimal(10,2) DEFAULT NULL,
-  PRIMARY KEY (`fd_id`),
-  KEY `userId_idx` (`user_id`),
-  CONSTRAINT `userIdfd` FOREIGN KEY (`user_id`) REFERENCES `user_login` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb4 */ ;
-/*!50003 SET character_set_results = utf8mb4 */ ;
-/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
+  `fd_dur` int(11) DEFAULT NULL,
+  `compounding` int(11) DEFAULT 1,
+  `fd_return` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+--
+-- Triggers `fd`
+--
+DELIMITER $$
 CREATE TRIGGER `fd_BEFORE_INSERT` BEFORE INSERT ON `fd` FOR EACH ROW BEGIN
 DECLARE a DECIMAL(10,4);
 DECLARE b DECIMAL(10,4);
@@ -65,35 +60,17 @@ DECLARE c DECIMAL(10,4);
 DECLARE d DECIMAL(10,4);
 DECLARE e DECIMAL(10,4);
 DECLARE f DECIMAL(10,4);
-DECLARE i int;
-SET i=0;
 SET d=1;
 SET c = NEW.compounding * NEW.fd_dur;
 SET f = NEW.fd_rate / 100;
 SET a = f / NEW.compounding;
 SET b = 1 + a;
-WHILE i<c DO
-	BEGIN
-		Set d = d * b;
-		Set i  = i + 1;
-	END;
-    END WHILE;
+SET d  = POW(b,c);
 SET NEW.fd_return = NEW.fd_prin * d;
-END;;
+END
+$$
 DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb4 */ ;
-/*!50003 SET character_set_results = utf8mb4 */ ;
-/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
+DELIMITER $$
 CREATE TRIGGER `fd_BEFORE_UPDATE` BEFORE UPDATE ON `fd` FOR EACH ROW BEGIN
 DECLARE a DECIMAL(10,4);
 DECLARE b DECIMAL(10,4);
@@ -101,56 +78,37 @@ DECLARE c DECIMAL(10,4);
 DECLARE d DECIMAL(10,4);
 DECLARE e DECIMAL(10,4);
 DECLARE f DECIMAL(10,4);
-DECLARE i int;
-SET i=0;
 SET d=1;
 SET c = NEW.compounding * NEW.fd_dur;
 SET f = NEW.fd_rate / 100;
 SET a = f / NEW.compounding;
 SET b = 1 + a;
-WHILE i<c DO
-	BEGIN
-		Set d = d * b;
-		Set i  = i + 1;
-	END;
-    END WHILE;
+SET d  = POW(b,c);
 SET NEW.fd_return = NEW.fd_prin * d;
-END;;
+END
+$$
 DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `ppf`
 --
 
-DROP TABLE IF EXISTS `ppf`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `ppf` (
-  `ppf_id` int NOT NULL AUTO_INCREMENT,
-  `user_id` int NOT NULL,
-  `bank_name` varchar(70) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `ppf_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `bank_name` varchar(70) NOT NULL,
   `ppf_prin` decimal(10,2) DEFAULT NULL,
   `ppf_rate` decimal(10,2) DEFAULT NULL,
-  `ppf_year` decimal(10,2) DEFAULT '15.00',
-  `ppf_return` decimal(10,2) DEFAULT NULL,
-  PRIMARY KEY (`ppf_id`),
-  KEY `userIdppf_idx` (`user_id`),
-  CONSTRAINT `userIdppf` FOREIGN KEY (`user_id`) REFERENCES `user_login` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb4 */ ;
-/*!50003 SET character_set_results = utf8mb4 */ ;
-/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
+  `ppf_year` decimal(10,2) DEFAULT 15.00,
+  `ppf_return` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Triggers `ppf`
+--
+DELIMITER $$
 CREATE TRIGGER `ppf_BEFORE_INSERT` BEFORE INSERT ON `ppf` FOR EACH ROW BEGIN
 DECLARE si DECIMAL(10,2);
 DECLARE i DECIMAL(10,2);
@@ -178,21 +136,10 @@ SET si=p*r;
 SET si=si/100;
 SET a=p+si;
 SET NEW.ppf_return=a;
-END;;
+END
+$$
 DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb4 */ ;
-/*!50003 SET character_set_results = utf8mb4 */ ;
-/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
+DELIMITER $$
 CREATE TRIGGER `ppf_BEFORE_UPDATE` BEFORE UPDATE ON `ppf` FOR EACH ROW BEGIN
 DECLARE si DECIMAL(10,2);
 DECLARE i DECIMAL(10,2);
@@ -220,49 +167,39 @@ SET si=p*r;
 SET si=si/100;
 SET a=p+si;
 SET NEW.ppf_return=a;
-END;;
+END
+$$
 DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `rd`
 --
 
-DROP TABLE IF EXISTS `rd`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `rd` (
-  `rd_id` int NOT NULL AUTO_INCREMENT,
-  `user_id` int NOT NULL,
+  `rd_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `bank_name` varchar(45) DEFAULT NULL,
   `rd_prin` decimal(10,2) DEFAULT NULL,
   `rd_tenure` decimal(10,2) DEFAULT NULL,
   `rd_rate` decimal(10,2) DEFAULT NULL,
-  `rd_return` decimal(10,2) DEFAULT NULL,
-  PRIMARY KEY (`rd_id`),
-  KEY `userIdrd_idx` (`user_id`),
-  CONSTRAINT `userIdrd` FOREIGN KEY (`user_id`) REFERENCES `user_login` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb4 */ ;
-/*!50003 SET character_set_results = utf8mb4 */ ;
-/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
+  `rd_return` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Triggers `rd`
+--
+DELIMITER $$
 CREATE TRIGGER `rd_BEFORE_INSERT` BEFORE INSERT ON `rd` FOR EACH ROW BEGIN
 DECLARE i DECIMAL(10,4);
 DECLARE n INT;
 DECLARE a DECIMAL(10,4);
 DECLARE b DECIMAL(10,4);
-DECLARE c DECIMAL(10,4);
-DECLARE d DECIMAL(10,4);
+DECLARE c DECIMAL(10,6);
+DECLARE d DECIMAL(10,6);
+DECLARE p DECIMAL(10,4);
+SET p = NEW.rd_prin;
 SET n = NEW.rd_tenure*4;
 SET i=NEW.rd_rate/400;
 SET i=1+i;
@@ -271,77 +208,55 @@ SET d=i;
 CALL neg_expo(d);
 SET c=1-d;
 SET b=a-1;
-SET b=b*NEW.rd_prin;
+SET b=b*p;
 SET NEW.rd_return = b/c;
-END;;
+END
+$$
 DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb4 */ ;
-/*!50003 SET character_set_results = utf8mb4 */ ;
-/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
+DELIMITER $$
 CREATE TRIGGER `rd_BEFORE_UPDATE` BEFORE UPDATE ON `rd` FOR EACH ROW BEGIN
 DECLARE i DECIMAL(10,4);
 DECLARE n INT;
 DECLARE a DECIMAL(10,4);
 DECLARE b DECIMAL(10,4);
-DECLARE c DECIMAL(10,4);
-DECLARE d DECIMAL(10,4);
+DECLARE c DECIMAL(10,6);
+DECLARE d DECIMAL(10,6);
+DECLARE p DECIMAL(10,4);
+SET p = NEW.rd_prin;
 SET n = NEW.rd_tenure*4;
 SET i=NEW.rd_rate/400;
 SET i=1+i;
 SET a = POW(i,n);
-SET d=i;
-CALL neg_expo(d);
+SET d= POW(i,-1/3);
 SET c=1-d;
 SET b=a-1;
-SET b=b*NEW.rd_prin;
+SET b=b*p;
 SET NEW.rd_return = b/c;
-END;;
+END
+$$
 DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `stocks`
 --
 
-DROP TABLE IF EXISTS `stocks`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `stocks` (
-  `stock_id` int NOT NULL AUTO_INCREMENT,
-  `user_id` int NOT NULL,
+  `stock_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `stock_name` varchar(45) NOT NULL,
   `open_price` decimal(10,2) DEFAULT NULL,
   `close_price` decimal(10,2) DEFAULT NULL,
   `dividend` decimal(10,2) DEFAULT NULL,
   `stock_return` decimal(10,2) DEFAULT NULL,
-  `volume` decimal(10,2) DEFAULT NULL,
-  PRIMARY KEY (`stock_id`),
-  KEY `userId_idx` (`user_id`),
-  CONSTRAINT `userIdstk` FOREIGN KEY (`user_id`) REFERENCES `user_login` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb4 */ ;
-/*!50003 SET character_set_results = utf8mb4 */ ;
-/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
+  `volume` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Triggers `stocks`
+--
+DELIMITER $$
 CREATE TRIGGER `stocks_BEFORE_INSERT` BEFORE INSERT ON `stocks` FOR EACH ROW BEGIN
 DECLARE a DECIMAL(10,4);
 DECLARE b DECIMAL(10,4);
@@ -350,21 +265,10 @@ SET a= NEW.close_price - NEW.open_price;
 SET b= a/NEW.open_price;
 SET c= b*100;
 SET NEW.stock_return = c + NEW.dividend;
-END ;;
+END
+$$
 DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb4 */ ;
-/*!50003 SET character_set_results = utf8mb4 */ ;
-/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
+DELIMITER $$
 CREATE TRIGGER `stocks_BEFORE_UPDATE` BEFORE UPDATE ON `stocks` FOR EACH ROW BEGIN
 DECLARE a DECIMAL(10,4);
 DECLARE b DECIMAL(10,4);
@@ -373,39 +277,130 @@ SET a= NEW.close_price - NEW.open_price;
 SET b= a/NEW.open_price;
 SET c= b*100;
 SET NEW.stock_return = c + NEW.dividend;
-END;;
+END
+$$
 DELIMITER ;
 
---
--- Dumping routines for database 'investments'
---
-DROP PROCEDURE IF EXISTS `neg_expo`;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb4 */ ;
-/*!50003 SET character_set_results = utf8mb4 */ ;
-/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
-CREATE PROCEDURE `neg_expo`(INOUT var DECIMAL(10,4))
-BEGIN
-SET var = POW(var,-1/3);
-END ;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+-- --------------------------------------------------------
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Table structure for table `user_login`
+--
+
+CREATE TABLE `user_login` (
+  `user_id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `password` varchar(100) NOT NULL,
+  `is_active` tinyint(1) DEFAULT 1,
+  `is_admin` tinyint(1) NOT NULL DEFAULT 0,
+  `last_login_time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `created_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `fd`
+--
+ALTER TABLE `fd`
+  ADD PRIMARY KEY (`fd_id`),
+  ADD KEY `userId_idx` (`user_id`);
+
+--
+-- Indexes for table `ppf`
+--
+ALTER TABLE `ppf`
+  ADD PRIMARY KEY (`ppf_id`),
+  ADD KEY `userIdppf_idx` (`user_id`);
+
+--
+-- Indexes for table `rd`
+--
+ALTER TABLE `rd`
+  ADD PRIMARY KEY (`rd_id`),
+  ADD KEY `userIdrd_idx` (`user_id`);
+
+--
+-- Indexes for table `stocks`
+--
+ALTER TABLE `stocks`
+  ADD PRIMARY KEY (`stock_id`),
+  ADD KEY `userId_idx` (`user_id`);
+
+--
+-- Indexes for table `user_login`
+--
+ALTER TABLE `user_login`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `fd`
+--
+ALTER TABLE `fd`
+  MODIFY `fd_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `ppf`
+--
+ALTER TABLE `ppf`
+  MODIFY `ppf_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `rd`
+--
+ALTER TABLE `rd`
+  MODIFY `rd_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `stocks`
+--
+ALTER TABLE `stocks`
+  MODIFY `stock_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `user_login`
+--
+ALTER TABLE `user_login`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `fd`
+--
+ALTER TABLE `fd`
+  ADD CONSTRAINT `userIdfd` FOREIGN KEY (`user_id`) REFERENCES `user_login` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `ppf`
+--
+ALTER TABLE `ppf`
+  ADD CONSTRAINT `userIdppf` FOREIGN KEY (`user_id`) REFERENCES `user_login` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `rd`
+--
+ALTER TABLE `rd`
+  ADD CONSTRAINT `userIdrd` FOREIGN KEY (`user_id`) REFERENCES `user_login` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `stocks`
+--
+ALTER TABLE `stocks`
+  ADD CONSTRAINT `userIdstk` FOREIGN KEY (`user_id`) REFERENCES `user_login` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2022-01-15 23:50:05
